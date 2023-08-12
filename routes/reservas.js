e40bcdccb2dfec05a4b1f4dc3c2ba69aad3867e2
erickslk <erick.osorto@unitec.edu>
2023-08-11 20:26:45 -0600

    delete

--- routes/reservas.js
+++ routes/reservas.js
@@ -80,38 +80,25 @@ where fecha =$5
         });
 });
 
-laboratorio.delete('/:id', async (req, res) => {
-    try {
-        const sql = `
-            UPDATE tbl_reservas
-            SET activo = false, fecha_borrado = current_timestamp
-            WHERE id_reserva = $1
-            id_lab = $2
-            id_horario =$3
-            id_usuario = $4
-            fecha= $5
-            RETURNING id_reserva, fecha_borrado
-        `;
-        
-        const data = await db.oneOrNone(sql, [req.params.id]);
+laboratorio.delete('/:id_reserva', (req, res) => {
+    let sql = ` update tbl_reservas
+                set activo = false , 
+                    fecha_borrado = current_timestamp 
+                where id_reserva = $1 `;
 
-        if (data) {
-            res.json({
-                id_reserva: data.id_reserva,
-                id_lab: data.id_lab,
-                id_horario: data.id_horario,
-                id_usuario: data.id_usuario,
-                fecha : data.fecha,
-                activo: false,
-                fecha_borrado: data.fecha_borrado
-            });
-        } else {
-            res.status(404).json({ error: 'Registro no encontrado' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-    }
+    db.result(sql, [req.params.id_reserva] ,   r => r.rowCount)
+        .then(data => {
+
+            const objetoBorrado     = {  id_reserva : req.params.id_reserva, 
+                                        activo : false   };
+            
+            res.json(objetoBorrado);
+
+        })
+        .catch((error) => {
+            res.json(error);
+        });
+
 });
 
 module.exports = laboratorio;
