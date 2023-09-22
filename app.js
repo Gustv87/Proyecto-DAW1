bc2e3a9c53922cdcb6b5dc82abe82a1b78c5a7b7
gustavomartinez <gustavomartinezh87@gmail.com>
2023-09-21 21:08:00 -0600

    se agrego permisos de cors

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
 //Rutas
 const laboratorio = require('./routes/laboratorio.js');
 app.use('/api/laboratorio', laboratorio);
